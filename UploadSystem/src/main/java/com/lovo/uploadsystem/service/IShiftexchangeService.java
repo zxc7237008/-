4542aef6d92a06f3894f834b984199d279cc08a1
wenhao <343332420@qq.com>
2019-04-25 19:10:16 +0800

    wenhao

--- UploadSystem/src/main/java/com/lovo/uploadsystem/service/IShiftexchangeService.java
+++ UploadSystem/src/main/java/com/lovo/uploadsystem/service/IShiftexchangeService.java
@@ -1,8 +1,39 @@
 package com.lovo.uploadsystem.service;
 
+import java.util.List;
+
 import com.lovo.uploadsystem.entity.ShiftexchangeEntity;
 
 public interface IShiftexchangeService {
-
 	void savejournal(ShiftexchangeEntity shiftexchangeEntity);
+	
+	  /**
+   * 分页
+   * @param pageNum
+   * @param pageSize
+   * @return
+   */
+  List<ShiftexchangeEntity> showshiftexListPage(int pageNum,int pageSize);
+  
+  /**
+   * 有参分页
+   * @param too 参数
+   * @param pageNum
+   * @param pageSize
+   * @return
+   */
+  List<ShiftexchangeEntity> showshiftexListPageto(String too, int pageNum, int pageSize);
+  
+  /**
+   * 返回总条数
+   * @return
+   */
+	int getAllPage(int pageSize);
+	/**
+	 * 返回有参条数
+	 * @param too
+	 * @param pageSize
+	 * @return
+	 */
+	int getAllPageto(String too,int pageSize);
 }
