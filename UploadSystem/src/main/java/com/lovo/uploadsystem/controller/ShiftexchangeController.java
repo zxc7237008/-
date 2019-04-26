c27ca1c4d3d004942120079e995b7f190341b727
wenhao <343332420@qq.com>
2019-04-26 17:47:02 +0800

    wenhao

--- UploadSystem/src/main/java/com/lovo/uploadsystem/controller/ShiftexchangeController.java
+++ UploadSystem/src/main/java/com/lovo/uploadsystem/controller/ShiftexchangeController.java
@@ -2,17 +2,23 @@ package com.lovo.uploadsystem.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.lovo.uploadsystem.entity.JournalEntity;
 import com.lovo.uploadsystem.entity.PageBean;
 import com.lovo.uploadsystem.entity.ShiftexchangeEntity;
+import com.lovo.uploadsystem.entity.UserEntity;
 import com.lovo.uploadsystem.service.IShiftexchangeService;
 import com.netflix.client.http.HttpRequest;
 import com.sun.research.ws.wadl.Request;
@@ -25,40 +31,42 @@ public class ShiftexchangeController {
 	
 	private String Uname;
 	
-//	@Autowired
-//	private RestTemplate restTemplate;
-	
-//	@RequestMapping("getshiftexchange")
-//	public String getshiftexchange(String name,String password){
-//	return	restTemplate.getForEntity("http://*", String.class,name,password).getBody();
-//	}
-//	
-	
+
+	//交班按钮
 	@RequestMapping("shiftexchange")
-    public ModelAndView shiftexchange(String password){
+    public ModelAndView shiftexchange(String password,HttpServletRequest request){
 		 ModelAndView modelAndView=null;
-//		 String passwordto = this.getshiftexchange();
-		System.out.println(password);
-		Uname="wenhao";
-		if(password.equals(Uname)){
-         modelAndView = new ModelAndView("*");
-		}else{
-			modelAndView = new ModelAndView("*");
-		}
+	 	   HttpSession session =request.getSession(); 
+	 	   UserEntity userEntity = (UserEntity) session.getAttribute("User");
+	 	   System.out.println("交班方法里面"+userEntity.getUname());//占时不知道名字
+	 	   System.out.println("交班方法里面"+password);
+	 	   String userpassword= userEntity.getUpasss();
+		
+		
+		if(password.equals(userpassword)){
+	         modelAndView = new ModelAndView("login");
+	         //删除session里面存在的用户
+	         session.removeAttribute("User");
+			}else {
+				ModelAndView modelAndView1 = new ModelAndView();
+				RedirectView rv = new RedirectView("gotoJournal");//重定向的视图
+				modelAndView1.setView(rv);
+				 return modelAndView1;
+			}		
+		
+		System.out.println("返回了视图");
         return modelAndView;
     }
 	
 	@RequestMapping("saveshiftexchangecontent")
-    public ModelAndView saveshiftexchangecontent(String daily){
-//  	  HttpServletRequest request =((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-// 	   HttpSession session =request.getSession(); 
-// 	   UserEntity userEntity = (UserEntity) session.getAttribute("User");
-// 	   System.out.println(userEntity.getUname());//占时不知道名字
+    public ModelAndView saveshiftexchangecontent(String daily, HttpServletRequest request){
+		 
+ 	   HttpSession session =request.getSession(); 
+ 	   UserEntity userEntity = (UserEntity) session.getAttribute("User");
+ 	   System.out.println(userEntity.getUname());//占时不知道名字
 		 ModelAndView modelAndView=new ModelAndView();
-		System.out.println(daily);
-		Uname="duhao";
-		ShiftexchangeEntity shiftexchangeEntity = new ShiftexchangeEntity();
-		shiftexchangeEntity.setName(Uname);
+		 ShiftexchangeEntity shiftexchangeEntity = new ShiftexchangeEntity();
+		shiftexchangeEntity.setName(userEntity.getUname());	
 		shiftexchangeEntity.setDaily(daily);
 		iShiftexchangeService.savejournal(shiftexchangeEntity);
 			RedirectView rv = new RedirectView("gotoJournal");//重定向的视图
